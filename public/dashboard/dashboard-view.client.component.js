1fd4b4e48165aa501d196638747c4ac28c891b4c
unknown <tumbone@gmail.com>
2017-01-07 09:39:31 +0200

    cleaned up authentication handling on dashboard template

--- public/dashboard/dashboard-view.client.component.js
+++ public/dashboard/dashboard-view.client.component.js
@@ -3,9 +3,7 @@ module('dashboardView').
     component('dashboardView',{
         templateUrl: '/dashboard/dashboard-view.template.html',
         controller: ['UserAuthentication', function dashboardCtrl(UserAuthentication){
-            this.authentication = UserAuthentication;
-
-            console.log(this.authentication.user);
+            this.authentication = UserAuthentication;            
         }]
 
     });
