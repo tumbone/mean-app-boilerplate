e198f53f29937766c4d425f201ed55c72a22692c
unknown <tumbone@gmail.com>
2017-01-14 07:43:42 +0200

    added a production.js config file

---
+++ config/env/production.js
@@ -0,0 +1,13 @@
+var port = 1337;
+
+module.exports = {
+    port: port,
+    db: 'mongodb://localhost/todos',
+    twitter : {
+//        clientID: 'API Key',
+        clientID: 'LOub49i95Gtd7jBlH5UGh7MRC',
+//        clientSecret: 'API Secret',
+        clientSecret: 'uQkg6qQ6PhZ0LdLFbJ2VV6LMLmeHK42RAtNEDtZcLVbD0i9epe',
+        callback: '<<INSERT WEBSITE URL>>' '/oauth/twitter/callback'
+    }
+};
