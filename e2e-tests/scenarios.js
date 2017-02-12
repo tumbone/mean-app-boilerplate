5e30d7aa20ec6cedcc8399c8701069c7ee0e0d7a
tumbone <tumbonea@ovationsgroup.com>
2017-02-12 06:19:49 +0200

    added e2e testing environment

---
+++ e2e-tests/scenarios.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Mean-App Boilerplate', function() {
+  it('should open the dashboard.html page', function() {
+    browser.get('/');
+    // expect(browser.getLocationAbsUrl()).toBe('/');
+    expect(browser.geCurrentUrl).toBe('/');
+  });
+});
