00871a89ae31817cc5029b2b9a052bfcf7585fe2
tumbone <tumbonea@ovationsgroup.com>
2017-02-15 11:44:15 +0200

    Added e2e test to test login page

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,7 +3,17 @@
 describe('Mean-App Boilerplate', function() {
   it('should open the dashboard.html page', function() {
     browser.get('/');
-    // expect(browser.getLocationAbsUrl()).toBe('/');
-    expect(browser.geCurrentUrl).toBe('/');
+    expect(browser.getLocationAbsUrl()).toBe('/');
+    //expect(browser.geCurrentUrl()).toBe('/');
+  });
+
+
+  describe('View: Login', function(){
+
+    browser.get('login');
+    it('should display the login page', function(){
+      expect(browser.getLocationAbsUrl()).toBe('/');
+      //var login = element(by.model());
+    });
   });
 });
